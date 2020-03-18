73a91e3665ce91dbe2ba8cd457a1a24de059b92b
Olena Lukashchuk <s19155@pjwstk.edu.pl>
2020-03-18 21:34:50 +0100

    trying to add put and delete

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ccw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ccw3.Controllers
@@ -10,6 +11,9 @@ namespace ccw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
+
+        List<Student> studenci = new List<Student>();
+
         [HttpGet]
         public string GetStudent(string orderBy)
         {
@@ -30,6 +34,34 @@ namespace ccw3.Controllers
             }
 
             return NotFound("Nie ma takiego studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student st)
+        {
+
+            st.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            st.FirstName = "Lena";
+            st.LastName = "Do";
+            st.IdStudent = 1;
+            studenci.Add(st);
+            return Ok(st);
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult ChangeId(int id, int newId)
+        {
+            if(id != newId)
+            {
+                //studenci.GetEnumerator = newId;
+                // how to get something from list? there iss no get method...
+            }
+            else
+            {
+                return NoContent();
+            }
+
+            return Ok();
         }
 
 
