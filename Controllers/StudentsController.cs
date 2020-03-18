dd33bb0065db19c8937fdc4b1386f4049d1340a1
Olena Lukashchuk <s19155@pjwstk.edu.pl>
2020-03-18 20:04:36 +0100

    add GetStudent(id)

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -15,5 +15,23 @@ namespace ccw3.Controllers
         {
             return "washi wa";
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+
+            if (id == 1)
+            {
+                return Ok("washi wa Lena");
+            }
+            else if (id == 2)
+            {
+                return Ok("Washi wa lena de wa arimasen");
+            }
+
+            return NotFound("Nie ma takiego studenta");
+        }
+
+
     }
 }
