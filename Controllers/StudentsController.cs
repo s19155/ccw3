56fd29d4658aaeb80b3b40d1f48f2bda2af0f516
Olena Lukashchuk <s19155@pjwstk.edu.pl>
2020-03-18 19:26:00 +0100

    Добавьте файлы проекта.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ccw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "washi wa";
+        }
+    }
+}
